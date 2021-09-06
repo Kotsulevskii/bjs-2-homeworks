bfed1ff3cf4b5d3b2e33e208224d6a0ced6196f1
Sergey Kotsulevskiy <seriy_552@bk.ru>
2021-09-06 22:14:00 +0300

    first commit task1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,21 @@
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let arr = [];
+  let d = b * b - 4 * a * c;
+  if (d === 0) {
+    let x = -b / (2 * a);
+    arr.push(x);
+    console.log(arr);
+  }
+  else if (d > 0) {
+    let x1 = (-b + Math.sqrt(d)) / (2 * a);
+    let x2 = (-b - Math.sqrt(d)) / (2 * a);
+    arr.push(x1);
+    arr.push(x2);
+    console.log(arr);
+  } else {
+    console.log(arr);
+  }
+  return arr; 
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
