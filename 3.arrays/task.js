023355b7c146e09955b1fb48d70d3be1b887e348
Sergey Kotsulevskiy <seriy_552@bk.ru>
2021-12-02 19:00:31 +0300

    Task3 Initial

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,10 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
+  let result = arr1.every((element, index, arr) => arr1[element] === arr2[element] && arr1[index] === arr2[index]);
 
   return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
+  let resultArr = arr.filter((element) => element > 0 && element % 3 === 0).map((element) => element * 10);
   return resultArr; // array
 }
