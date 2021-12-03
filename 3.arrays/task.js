4661b3ec29defa907b9a28188baab51971c43e90
Sergey Kotsulevskiy <seriy_552@bk.ru>
2021-12-03 20:02:56 +0300

    Task3 Short text and .length

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,10 +1,7 @@
 function compareArrays(arr1, arr2) {
-  let result = arr1.every((element, index, arr) => arr1[element] === arr2[element] && arr1[index] === arr2[index]);
-
-  return result; // boolean
+  return arr1.every((element, index, arr) => arr1.length === arr2.length && arr1[index] === arr2[index]);
 }
 
 function advancedFilter(arr) {
-  let resultArr = arr.filter((element) => element > 0 && element % 3 === 0).map((element) => element * 10);
-  return resultArr; // array
+  return arr.filter((element) => element > 0 && element % 3 === 0).map((element) => element * 10);
 }
