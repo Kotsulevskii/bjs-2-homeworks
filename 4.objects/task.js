1bdb99be1c1d9bd78a67a3bab5bd08a7e727f986
Sergey Kotsulevskiy <seriy_552@bk.ru>
2021-12-03 23:21:13 +0300

    Task4 Initial commit

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,42 @@
 function Student(name, gender, age) {
     // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+Student.prototype.setSubject = function(subjectName) {
+  //Ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function(mark) {
+  if ( this.marks === undefined) {
+    this.marks = [mark]; 
+  } else {
+    this.marks.push(mark); 
+  }
+}
+Student.prototype.addMarks = function (...markArr) {
+    if (this.marks === undefined) {
+      this.marks = markArr;
+    } else {
+     this.marks = this.marks.concat(markArr);
+    }
+}
+Student.prototype.getAverage = function () {
+  //Ваш код
+  return this.marks.reduce((acc, item, idx) => {
+    if (idx === this.marks.length - 1) {
+      return ((acc + item) / this.marks.length);
+    } else {
+      return (acc + item);
+    }
+  })
+}
+Student.prototype.exclude = function(reason) {
+  //Ваш код
+  delete this.subject
+  delete this.marks
+  this.excluded = reason;
+}
